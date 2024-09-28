69565ece76eec8f912776c34974d2676e249c96c
Tommy Nguyen <nguyenanhnhat123456@gmail.com>
2024-09-28 14:59:06 +0700

    Fix content

--- frontend/src/constant/content.ts
+++ frontend/src/constant/content.ts
@@ -88,7 +88,7 @@ export const PAGE_CONTENT: any = {
     {
       title: "Streamvibe",
       description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        "I developed a movie website using a free Figma template and integrated TMDB APIs. The tech stack includes ReactJS, Redux Toolkit, ContextAPI, react-router-dom, Axios, Yarn, GitHub, and SASS/SCSS with styled-components. Key features include routing, movie displays, and a login/register system with multi-factor authentication for managing favorites and watchlists. Users can search, filter, and sort movies, with form validation via the useForm hook. I followed the Presentational/Container Component design pattern for maintainability. The site is deployed on Vercel for easy updates and accessibility.",
       image: "/images/streamvibe.png",
       srcLink: "https://github.com/TommyNhatNguyen/StreamVibe",
       webLink: "https://stream-vibe-tommynguyen.vercel.app/",
@@ -96,7 +96,7 @@ export const PAGE_CONTENT: any = {
     {
       title: "FinanceFlow",
       description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        "I created a fully responsive website using a Figma template from CFD Circle, leveraging technologies such as HTML, CSS, JavaScript ES6, SCSS/SASS, NPM, and Grunt. The project involved translating the Figma design into a functional website, incorporating simple form validation with plain JavaScript. Additionally, I implemented animations and interactive sections using JavaScript libraries like Flickity for carousels and AoS for scroll animations, ensuring an engaging user experience.",
       image: "/images/financeflow.png",
       srcLink: "https://github.com/TommyNhatNguyen/FinanceFlow",
       webLink: "https://finance-flow-rho.vercel.app/",
@@ -104,7 +104,7 @@ export const PAGE_CONTENT: any = {
     {
       title: "CFD Shop",
       description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        "I developed a React website using a template and APIs from CFD Circle, employing technologies like ReactJS, Redux Toolkit, Axios, and Ant Design. The project features JWT authentication for user login and registration, along with product searching, filtering, and cart management (add, remove, update). Users can review products, check out, search blogs, paginate content, and manage profiles. My role involved converting an HTML/CSS/JavaScript project into a React app, utilizing Redux Toolkit and ContextAPI for state management, and implementing Axios for REST API calls. I also used the useForm hook for client-side validation and styled components with styled-components for a reusable UI, optimizing performance with useMemo and lazy loading techniques.",
       image: "/images/cfdshop.png",
       srcLink: "https://github.com/TommyNhatNguyen/CFD-SHOP",
       webLink: "https://cfd-shop-nhatnguyen.vercel.app/",
@@ -112,7 +112,7 @@ export const PAGE_CONTENT: any = {
     {
       title: "DQH Architecture",
       description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        "I built this website using a Figma template provided by my colleague, employing a range of technologies including HTML, CSS, JavaScript ES6, SCSS/SASS, NPM, and Grunt. My tasks included creating a fully responsive website that adheres closely to the Figma design specifications, utilizing plain JavaScript for simple form validation alongside SCSS/SASS for styling.",
       image: "/images/dqharchitecture.png",
       srcLink: "https://github.com/TommyNhatNguyen/DQH-Architecture",
       webLink: "https://dqh-architecture.vercel.app/",
